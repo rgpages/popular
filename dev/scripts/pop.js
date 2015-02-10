a09f86465a5c23c680e19f8bc2ae44a79d05be81
Rob Denton <robdentonrg@gmail.com>
2015-02-10 09:54:43 -0800

    asdf

---
+++ dev/scripts/pop.js
@@ -0,0 +1,84 @@
+/**
+ * Popular
+ * Top 100 stories in the last 24 hours
+ *
+ * @author Micky Hulse
+ * @link http://mky.io
+ * @docs https://github.com/registerguard/popular
+ * @copyright Copyright (c) 2015 Micky Hulse.
+ * @license Released under the Copyright © The Register-Guard 2014. All rights reserved..
+ * @version 0.1.0
+ * @date 2015/02/10
+ */
+
+
+/**
+ * Popular
+ * Top 100 stories in the last 24 hours
+ *
+ * @author Micky Hulse
+ * @link http://mky.io
+ * @docs https://github.com/registerguard/popular
+ * @copyright Copyright (c) 2015 Micky Hulse.
+ * @license Released under the Copyright © The Register-Guard 2014. All rights reserved..
+ * @version 0.1.0
+ * @date 2015/02/10
+ */
+
+
+/**
+ * Popular
+ * Top 100 stories in the last 24 hours
+ *
+ * @author Micky Hulse
+ * @link http://mky.io
+ * @docs https://github.com/registerguard/popular
+ * @copyright Copyright (c) 2015 Micky Hulse.
+ * @license Released under the Copyright © The Register-Guard 2014. All rights reserved..
+ * @version 0.1.0
+ * @date 2015/02/10
+ */
+
+
+/**
+ * Singleton with "register" functionality.
+ *
+ * @see http://codereview.stackexchange.com/questions/15166/best-way-to-organize-javascript-for-website
+ */
+
+(function(exports) {
+	
+	'use strict';
+	
+	var initialized,
+	    registry = []; // Collection of module.
+	
+	// Adds module to collection:
+	exports.register = function(moduleDeclaration) {
+		
+		registry.push(moduleDeclaration); // Add module to registry.
+		
+		if (initialized) {
+			
+			// If lib already initialized, register and execute module immediately:
+			moduleDeclaration.call(this);
+			
+		}
+		
+	};
+	
+	// Executes every module:
+	exports.init = function() {
+		
+		initialized = true; // Flippin' switches!
+		
+		// Loop through each module and execute:
+		for (var i = 0, l = registry.length; i < l; i++) {
+			
+			registry[i].call(this); // Call and execute module.
+			
+		}
+		
+	};
+	
+}(window.POP = window.POP || {})); // Use existing namespace or make a new object of that namespace.
