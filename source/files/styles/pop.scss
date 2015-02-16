7599484d2e10781ab3acdb16d010e18dc0123e30
Rob Denton <robdentonrg@gmail.com>
2015-02-16 10:54:23 -0800

    update loading icon location

--- source/files/styles/pop.scss
+++ source/files/styles/pop.scss
@@ -78,6 +78,20 @@
  * @copyright Copyright (c) 2015 Micky Hulse.
  * @license Released under the Copyright © The Register-Guard 2014. All rights reserved..
  * @version 0.1.0
+ * @date 2015/02/16
+ */
+
+
+/**
+ * Popular
+ * Top 100 stories in the last 24 hours
+ *
+ * @author Micky Hulse
+ * @link http://mky.io
+ * @docs https://github.com/registerguard/popular
+ * @copyright Copyright (c) 2015 Micky Hulse.
+ * @license Released under the Copyright © The Register-Guard 2014. All rights reserved..
+ * @version 0.1.0
  * @date 2015/02/12
  */
 
